db7de84d9f34a380ea10ea2ded5df53518f282c3
Luigi Medrano <luigimedrano03@gmail.com>
2024-10-20 21:34:20 -0500

    updated hw site

--- task3/my_custom_app/src/components/HardwareSet.js
+++ task3/my_custom_app/src/components/HardwareSet.js
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, Typography, TextField, Button } from '@mui/material';
 
 const HardwareSet = ({ hwSet, onCheckInOut }) => {
+  const [quantity, setQuantity] = useState('');
+
+  const handleCheckInOut = (action) => {
+    if (!quantity) return;
+    onCheckInOut(action, parseInt(quantity));
+    setQuantity('');
+  };
+
   return (
     <Box display="flex" alignItems="center" mt={1}>
       <Typography variant="body2" sx={{ flexGrow: 1 }}>
         {hwSet.name}: {hwSet.available}/{hwSet.total}
       </Typography>
       <TextField 
+        value={quantity}
+        onChange={(e) => setQuantity(e.target.value)}
         size="small" 
         label="Enter qty" 
+        type="number"
         variant="outlined" 
         sx={{ width: 100, mr: 1 }}
       />
-      <Button variant="outlined" size="small" onClick={() => onCheckInOut('Check In')}>
+      <Button variant="outlined" size="small" onClick={() => handleCheckInOut('Check In')}>
         Check In
       </Button>
-      <Button variant="outlined" size="small" onClick={() => onCheckInOut('Check Out')}>
+      <Button variant="outlined" size="small" onClick={() => handleCheckInOut('Check Out')}>
         Check Out
       </Button>
     </Box>
